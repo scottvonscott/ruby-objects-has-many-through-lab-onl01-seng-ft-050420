8d99f30e0a259f592092d4513b2f424228230fd9
Scott  Keplinger <skeplin17@gmail.com>
2020-05-20 03:10:11 +0000

    Automatically backed up by Learn

---
+++ lib/appointment.rb
@@ -0,0 +1,25 @@
+class Appointment
+
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize (name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def appointments
+  end
+
+  def new_appointment
+  end
+
+  def patients
+  end
+
+end
