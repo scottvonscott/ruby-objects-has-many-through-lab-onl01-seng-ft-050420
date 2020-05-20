07d9c2f6a4cc62915bf3f373218cc53f201d1ffa
Scott  Keplinger <skeplin17@gmail.com>
2020-05-20 03:55:44 +0000

    Done.

---
+++ lib/appointment.rb
@@ -0,0 +1,26 @@
+class Appointment
+
+  attr_accessor :date, :patient, :doctor
+
+  @@all = []
+
+  def initialize (date, patient, doctor)
+    @date = date
+    @patient = patient
+    @doctor = doctor
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def doctor
+    @doctor
+  end
+
+  def patient
+    @patient
+  end
+
+end
